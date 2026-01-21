dd1039afa026a9330a43ec440b4b40c89686768c
CaelSerrao <CaelSerrao>
2026-01-21 17:31:31 -0400

    added basic flagging

--- predict.py
+++ predict.py
@@ -4,7 +4,12 @@ Can be Mp3, Wav, etc
 """
 import whisper
 
-model = whisper.load_model("tiny.en")
+model = whisper.load_model("small.en")
 result = model.transcribe("Beautiful_Now(256k).mp3")
+keywords = ["moment"]
+for word in keywords:
+    if word in result["text"].lower():
+        print ("Bad words detected")
 
-print(type(result))
+
+
