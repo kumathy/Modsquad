e7d6c3e59aaf3a3b8ae5d9fdac7716801af072c2
anh-tran2106 <tranlamtuananh2003@gmail.com>
2026-02-03 22:47:00 -0400

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react';
 
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Separator } from '@/components/ui/separator';
 
 export default function App() {
   
   return (
-    <div className="p-6">
-      <Card>
-        <h2>Welcome to ModSquad</h2>
-        <Button>Launch</Button>
-      </Card>
+    <div className='min-h-screen'>
+      <header className='border-b'>
+        <div className="container mx-auto px-4 py-6">
+          <h1 className='scroll-m-20 text-3xl font-bold tracking-tight text-balance'>Modsquad</h1>
+          <p className='leading-7 text-muted-foreground'>
+          Stream Moderation with AI-Powered Voice Cloning
+          </p>
+        </div>
+      </header>
     </div>
   )
 }
